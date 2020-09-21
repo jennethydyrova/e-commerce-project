3c02952eccd76f475c2cd1021a046a5855a782b0
Hiba <hiba.mashfij@gmail.com>
2020-09-21 16:02:26 +0300

    working on main page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
-import Products from "./products";
+import Products  from "./products";
+import NavBar from "./NavBar";
+import Page from "./MainPage";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div>
+      <NavBar />
+      <Page />
       <Products />
     </div>
   );
